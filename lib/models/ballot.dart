ded349668038594c9eb5df187210442513d4ce5f
Christian Emil Stender <90451897+Stender98@users.noreply.github.com>
2023-02-10 12:18:18 +0100

    Update ballot.dart

--- lib/models/ballot.dart
+++ lib/models/ballot.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:mobile_voting_verifier/models/multi_ciphertext.dart';
 import 'package:mobile_voting_verifier/models/proof.dart';
 

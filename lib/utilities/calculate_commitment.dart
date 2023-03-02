34a79fcb04e10b31a5ce4324c0a3c2cb219fafd1
ches <ches@itu.dk>
2023-03-02 15:41:57 +0100

    moved calculate commit to utilities

---
+++ lib/utilities/calculate_commitment.dart
@@ -0,0 +1,5 @@
+import 'package:mobile_voting_verifier/models/challenge_request.dart';
+
+Future<(String, ChallengeRequest)> calculateChallengeCommitment() async {
+  return throw UnimplementedError();
+}

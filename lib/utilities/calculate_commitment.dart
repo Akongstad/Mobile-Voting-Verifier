46dca07b9d8801a7274adb6a130fe7bb1a91986e
stender98 <stender98@gmail.com>
2023-03-02 18:45:03 +0100

    refactored to use api_calls.dart

--- lib/utilities/calculate_commitment.dart
+++ lib/utilities/calculate_commitment.dart
@@ -1,5 +1,6 @@
 import 'package:mobile_voting_verifier/models/challenge_request.dart';
 
 Future<(String, ChallengeRequest)> calculateChallengeCommitment() async {
-  return throw UnimplementedError();
+  return throw UnimplementedError(); 
+  //The String is the challenge commitment c and the ChallengeRequest is the sampled BigInt e and BigInt r, which should be saved for later use. 
 }

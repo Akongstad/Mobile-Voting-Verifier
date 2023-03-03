d462087ca67604f26f85175f2a19af717bff1d9b
ches <ches@itu.dk>
2023-03-03 12:06:50 +0100

    testing elliptic package

--- lib/utilities/calculate_commitment.dart
+++ lib/utilities/calculate_commitment.dart
@@ -1,5 +1,15 @@
 import 'package:mobile_voting_verifier/models/challenge_request.dart';
+import 'package:elliptic/elliptic.dart';
 
 Future<(String, ChallengeRequest)> calculateChallengeCommitment() async {
-  return throw UnimplementedError();
+  var ecurve1 = getSecp256k1();
+  var ecurve2 = getSecp256k1();
+
+  var e = ecurve1.a;
+  var r = ecurve2.a;
+  
+
+
+  return ("Test", ChallengeRequest(challenge: e, challengeRandomCoin: r));
+  //return throw UnimplementedError();
 }

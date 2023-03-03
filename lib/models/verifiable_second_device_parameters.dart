03ce52943fe113c246e3d0ee85fab0d45525ecb7
Akongstad <kongstad@outlook.com>
2023-03-03 12:09:38 +0100

    - add dart crypto package - add verifyHash to verifiable_second_device_parameters.dart - add public_parameters.json to store preconfigured params

--- lib/models/verifiable_second_device_parameters.dart
+++ lib/models/verifiable_second_device_parameters.dart
@@ -1,3 +1,8 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:crypto/crypto.dart';
+import 'package:flutter/services.dart';
+
 import 'package:mobile_voting_verifier/models/core_3_ballot.dart';
 
 class VerifiableSecondDeviceParameters {
@@ -15,5 +20,36 @@ class VerifiableSecondDeviceParameters {
       : publicKey = json["publicKey"],
         verificationKey = json["verificationKey"],
         ballots = (json["ballots"] as List)
-            .map((e) => Core3Ballot.fromJson(e)).toList();
+            .map((e) => Core3Ballot.fromJson(e))
+            .toList();
+
+  factory VerifiableSecondDeviceParameters.fromString(String jsonString) =>
+      VerifiableSecondDeviceParameters.fromJson(json.decode(jsonString));
+
+  /*
+  * Read jsonObject from file.
+  * Used with verify hash to verify public parameters fingerprint.
+  */
+  static Future<Map<String, dynamic>> jsonFromFile(String filepath) async {
+    try{
+      var publicParametersString = await rootBundle.loadString(filepath);
+      return jsonDecode(publicParametersString);
+    } catch (e) {
+      print(e);
+      return throw FileSystemException("Failed to read or locate public_parameters.json", filepath);
+    }
+  }
+
+  /*
+  * The verification procedure check 1
+  * The fingerprint FINGERPRINT is in fact the SHA256 hash of PUBLIC_PARAMETERS_JSON
+  */
+  static Future<bool> verifyHash() async {
+    final paramsJson = await jsonFromFile("assets/public_parameters.json");
+    var paramsBytes = utf8.encode(paramsJson["publicParametersJson"]);
+    const Hash algorithm = sha256;
+    var paramsSHA256 = algorithm.convert(paramsBytes);
+
+    return paramsSHA256.toString() == paramsJson["fingerprint"];
+  }
 }

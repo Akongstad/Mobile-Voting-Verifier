eb84a570d7595e3176bfc104d91202c666fcc585
ches <ches@itu.dk>
2023-03-06 17:14:25 +0100

    outputs hexstring of correct length but not value

--- lib/utilities/calculate_commitment.dart
+++ lib/utilities/calculate_commitment.dart
@@ -1,6 +1,39 @@
+import 'dart:convert';
+import 'package:crypto/crypto.dart';
+
 import 'package:mobile_voting_verifier/models/challenge_request.dart';
 
-Future<(String, ChallengeRequest)> calculateChallengeCommitment() async {
+/* Future<(String, ChallengeRequest)> calculateChallengeCommitment() async {
   return throw UnimplementedError(); 
   //The String is the challenge commitment c and the ChallengeRequest is the sampled BigInt e and BigInt r, which should be saved for later use. 
+} */
+
+String keyDerivationFunction(
+    int returnLength, String initialSeed, String label, String context) {
+  var initialSeedEncoded = utf8.encode(initialSeed);
+  var labelEncoded = utf8.encode(label);
+  var contextEncoded = utf8.encode(context);
+
+  var hmacSha512 = Hmac(sha512, initialSeedEncoded);
+  var returnList = List.empty(growable: true);
+
+  for (var i = 0; i < (returnLength / 64).ceil(); i++) {
+    returnList.add(hmacSha512
+        .convert([
+          [i],
+          labelEncoded,
+          [0x00],
+          contextEncoded,
+          [returnLength]
+        ].expand((x) => x).toList())
+        .toString());
+  }
+
+  var returnString = '';
+  for (var element in returnList.sublist(0, (returnLength / 64).ceil() - 1)) {
+    returnString += element;
+  }
+
+  return returnString +
+      returnList.last.toString().substring(0, (returnLength % 64) * 2);
 }
--- test/utilities_tests/calculate_commitment_test.dart
+++ test/utilities_tests/calculate_commitment_test.dart
@@ -1,9 +1,23 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mobile_voting_verifier/models/challenge_request.dart';
 import 'package:mobile_voting_verifier/utilities/calculate_commitment.dart';
 
 void main() {
   group("test calculateCommitment()", () {
-    test('check ChallengeRequest', () async {});
+    test('check keyDerivationFunction from example', () async {
+      var initialSeed = "kdk";
+      var returnLength = 65;
+      var label = "label";
+      var context = "context";
+
+      var actual =
+          keyDerivationFunction(returnLength, initialSeed, label, context);
+      var expected =
+          "32 88 92 2A 96 65 33 C7 93 ED 53 20 45 FF FC 3C E6 BA 77 F2 7E 8F 60 C9 A3 D8 22 21 D8 6F 51 DD A0 07 36 DB A3 F8 AE 1D 94 B1 75 62 E8 38 D5 7F B8 54 00 D1 47 C6 E9 58 5E D4 D8 59 E4 61 20 B2 75"
+              .toLowerCase()
+              .replaceAll(' ', '');
+
+      expect(actual.length, expected.length);
+      expect(actual, expected);
+    });
   });
 }

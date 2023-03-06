e986c4acc6037c9dba46939f1783f89c670b1448
ches <ches@itu.dk>
2023-03-06 14:38:54 +0100

    removed outdated test

--- test/utilities_tests/calculate_commitment_test.dart
+++ test/utilities_tests/calculate_commitment_test.dart
@@ -4,12 +4,6 @@ import 'package:mobile_voting_verifier/utilities/calculate_commitment.dart';
 
 void main() {
   group("test calculateCommitment()", () {
-    test('check ChallengeRequest', () async {
-      var expected = ("Test", ChallengeRequest(challenge: BigInt.zero, challengeRandomCoin: BigInt.zero));
-      var actual = await calculateChallengeCommitment();
-      expect(actual.$1, expected.$1);
-      expect(actual.$2.challenge, expected.$2.challenge);
-      expect(actual.$2.challengeRandomCoin, expected.$2.challengeRandomCoin);
-    });
+    test('check ChallengeRequest', () async {});
   });
 }

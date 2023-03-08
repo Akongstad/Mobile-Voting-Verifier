7d4f13f9905b9b814f9771745f322dfea68c07e6
Akongstad <kongstad@outlook.com>
2023-03-08 11:50:41 +0100

    - add enable records to flutter.yml

--- lib/utilities/cryptography.dart
+++ lib/utilities/cryptography.dart
@@ -27,7 +27,7 @@ Future<BigInt> _numberFromSeedAsync((int, List<int>, int) lSeedI) async  {
   final i = lSeedI.$3;
   final k = seed + _int32ToBytesBigEndian(i);
 
-  final input = keyDerivationFunction(l, k, utf8.encode("generator"), utf8.encode("Polyas"));
+  final input = keyDerivationFunction((l/8).ceil(), k, utf8.encode("generator"), utf8.encode("Polyas"));
 
   var binArray = input   //O(the number fo bytes)!!!!
       .map((e) => e.toRadixString(2).padLeft(8, "0"))
@@ -50,46 +50,19 @@ Future<BigInt> _numberFromSeedAsync((int, List<int>, int) lSeedI) async  {
   return n;
 }
 
-Stream<BigInt> numbersFromSeedAsync(int l, List<int> seed) async* {
+Stream<Future<BigInt>> numbersFromSeedAsync(int l, List<int> seed) async* {
   var max = pow(2, l);
   var guard = max <= 0 ? (1 << 31) : max;
    for (int i = 1; i < 100000; i++) {
-     yield await _numberFromSeedAsync((l, seed, i));
+     yield _numberFromSeedAsync((l, seed, i));
   }
 }
 
-
-Iterable<BigInt> numbersFromSeed(int l, List<int> seed) sync* {
-  //Guards: 2^l and maxint32
+Stream<BigInt> numbersFromSeedAsync2(int l, List<int> seed) async* {
   var max = pow(2, l);
   var guard = max <= 0 ? (1 << 31) : max;
-  for (int i = 1; i < 1000; i++) {
-    //Concat seed and i: seed||i
-    var k = seed.join("") + i.toString();
-    //String byteArray =  keyDerivationFunction( (l/8).ceil(), k, "generator", "POLYAS");
-    //
-    var input =
-        "32 88 92 2A 96 65 33 C7 93 ED 53 20 45 FF FC 3C E6 BA 77 F2 7E 8F 60 C9 A3 D8 22 21 D8 6F 51 DD A0 07 36 DB A3 F8 AE 1D 94 B1 75 62 E8 38 D5 7F B8 54 00 D1 47 C6 E9 58 5E D4 D8 59 E4 61 20 B2 75";
-    var byteArray = input.split(" ");
-    var binArray = byteArray
-        .map((e) => int.parse(e, radix: 16).toRadixString(2).padLeft(8, "0"))
-        .join();
-    var bitLenght = byteArray.length * 8;
-    var excessBits = bitLenght - l;
-    if (excessBits > 0) {
-      binArray.replaceRange(0, excessBits, "");
-    }
-
-    //Padding for  bytearray to make sure bigint t is not interpreted as a negative integer
-    var pad = "0";
-    binArray = pad + binArray;
-    assert(binArray.length == l + pad.length);
-
-    var n = BigInt.tryParse(binArray, radix: 2);
-    if (n == null) {
-      throw ArgumentError.value(byteArray);
-    }
-    yield n;
+  for (int i = 1; i < 100000; i++) {
+    yield await _numberFromSeedAsync((l, seed, i));
   }
 }
 
--- test/utilities_tests/cryptography_test.dart
+++ test/utilities_tests/cryptography_test.dart
@@ -3,23 +3,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mobile_voting_verifier/utilities/cryptography.dart';
 
 void main() {
-  group("numbersFromSeed tests", () {
-    var l = 520;
-    var seed = utf8.encode("xyz");
-    BigInt a1 = BigInt.parse("1732501504205220402900929820446308723705652945081825598593993913145942097001127020633138020218038968109094917857329663184563374015879596834703721749398989648");
-    BigInt a2 = BigInt.parse("2207401303665503434031531355511922974889692817601183500259263742625914061046146142929376778072827450461936300533206904979740474482058840003720379960491023511");
-    BigInt a3 = BigInt.parse("1883889587903519477357838514223953979954201344665681798367023196328721975720052153913582122151913785273222921786889836987731296728825119604809609410157987402");
-    BigInt a4 = BigInt.parse("1423259849467217711185874799515607842842602785767879766623736284680209832704638390900412597196948750015976271793930713744890547611655064835165883323889981463");
-
-    BigInt a5 = BigInt.parse("300889668866034139069124727889607448675321904420316939286422373409404958717979350271745428651492739692008689990629401024794340355677791213941");
-    test('check numbersFromSeed from example', () async {
-      var sw = Stopwatch();
-      sw.start();
-      var actual = numbersFromSeed(l, seed).toList();
-      var elapsed = sw.elapsed; // elapsed on 100 elements. 0:00:00.014314
-      expect(actual.first, a1);
-    });
-  });
   group("numbersFromSeedAsync tests", () {
     var l = 520;
     var seed = utf8.encode("xyz");
@@ -32,10 +15,16 @@ void main() {
     test('check numbersFromSeedAsync from example', () async {
       var sw = Stopwatch();
       sw.start();
-      var actual = await numbersFromSeedAsync(l, seed).first;
-      var full = await numbersFromSeedAsync(l, seed).toList();
+      var actual = await numbersFromSeedAsync2(l, seed).toList();
+      var first =  actual.first;
+      var second =  actual[1];
+      var third =  actual[2];
+      var fourth =  actual[3];
       var elapsed = sw.elapsed; //100.000 elements: 0:00:04.463883
-      expect(actual, a1);
+      expect(first, a1);
+      expect(second, a2);
+      expect(third, a3);
+      expect(fourth, a4);
     });
   });
 }

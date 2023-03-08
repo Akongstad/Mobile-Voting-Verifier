033c75551150b3cabe8e2881e8f9d7a01e54f86a
Akongstad <kongstad@outlook.com>
2023-03-08 15:02:46 +0100

    Create function: DecodeECPoint() decryption requires converting a group element back into Zq - Test DecodeECPoint

--- lib/utilities/calculate_commitment.dart
+++ lib/utilities/calculate_commitment.dart
@@ -1,5 +1,9 @@
 import 'package:mobile_voting_verifier/models/challenge_request.dart';
 
 Future<(String, ChallengeRequest)> calculateChallengeCommitment() async {
-  return throw UnimplementedError();  
+  return throw UnimplementedError();
 }
+
+// floor is default for bigint division
+Future<BigInt> decodeECPoint(BigInt x, BigInt y, {int k = 80}) async =>
+    (x - BigInt.one) ~/ BigInt.from(k);

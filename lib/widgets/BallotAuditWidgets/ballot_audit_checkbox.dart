7733f2006cdbd021e6b01dff3b46a5f9226851d2
stender98 <stender98@gmail.com>
2023-03-24 12:49:58 +0100

    spelling

--- lib/widgets/BallotAuditWidgets/ballot_audit_checkbox.dart
+++ lib/widgets/BallotAuditWidgets/ballot_audit_checkbox.dart
@@ -3,9 +3,9 @@ import 'package:flutter/material.dart';
 class BallotAuditCheckBox extends StatefulWidget {
   const BallotAuditCheckBox({Key? key, required this.checkedMap,required this.id,
    this.title = "Beware! This will not change your vote",
-    this.content = "Q: Why may i edit my ballot?\n"
+    this.content = "Q: Why may I edit my ballot?\n"
         "A: To prevent voter coercion and vote selling.\n\n"
-        "Q: What will happen if i edit my ballot?\n"
+        "Q: What will happen if I edit my ballot?\n"
         "A: This will have no effect on the vote you cast."}) : super(key: key);
   final Map<String, bool> checkedMap;
   final String id;
